88df24d82e4c93709fac8dc7016b7aad96eed05b
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-10-17 17:31:51 +0530

    socials & about

--- components/Socials.js
+++ components/Socials.js
@@ -1,12 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 function Socials() {
   return (
     <div className="flex space-x-5 lg:space-x-6 items-center text-zinc-400 text-lg lg:text-xl">
-      <i className="bi bi-github"></i>
-      <i className="bi bi-linkedin"></i>
-      <i className="bi bi-instagram"></i>
-      <i className="bi bi-twitter"></i>
+      <Link href="https://github.com/priyangsubanerjee">
+        <i className="bi bi-github"></i>
+      </Link>
+      <Link href="https://www.linkedin.com/in/priyangsu-banerjee/">
+        <i className="bi bi-linkedin"></i>
+      </Link>
+      <Link href="https://www.instagram.com/priyangsu__banerjee/">
+        <i className="bi bi-instagram"></i>
+      </Link>
+      <Link href="https://twitter.com/oxygenated_fire">
+        <i className="bi bi-twitter"></i>
+      </Link>
     </div>
   );
 }
