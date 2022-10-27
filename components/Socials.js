59fedd4c8898590f7a8150ac5087571439e6c256
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-10-27 22:20:03 +0530

    hover

--- components/Socials.js
+++ components/Socials.js
@@ -5,16 +5,16 @@ function Socials() {
   return (
     <div className="flex space-x-5 lg:space-x-6 items-center text-zinc-400 text-lg lg:text-xl">
       <Link href="https://github.com/priyangsubanerjee">
-        <i className="bi bi-github"></i>
+        <i className="bi bi-github transition-all hover:text-zinc-100 cursor-pointer"></i>
       </Link>
       <Link href="https://www.linkedin.com/in/priyangsu-banerjee/">
-        <i className="bi bi-linkedin"></i>
+        <i className="bi bi-linkedin transition-all hover:text-zinc-100 cursor-pointer"></i>
       </Link>
       <Link href="https://www.instagram.com/priyangsu__banerjee/">
-        <i className="bi bi-instagram"></i>
+        <i className="bi bi-instagram transition-all hover:text-zinc-100 cursor-pointer"></i>
       </Link>
       <Link href="https://twitter.com/oxygenated_fire">
-        <i className="bi bi-twitter"></i>
+        <i className="bi bi-twitter transition-all hover:text-zinc-100 cursor-pointer"></i>
       </Link>
     </div>
   );
