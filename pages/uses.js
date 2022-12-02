c2fec378c03b2719830610b63994e0da16561dc3
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-12-02 20:38:49 +0530

    dark mode uses

--- pages/uses.js
+++ pages/uses.js
@@ -22,22 +22,19 @@ function Uses() {
           <div class="space-y-20">
             <section
               aria-labelledby=":r0:"
-              class="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40"
+              class="md:border-l md:border-zinc-700/40 md:pl-6"
             >
               <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
-                <h2
-                  id=":r0:"
-                  class="text-sm font-semibold text-zinc-800 dark:text-zinc-100"
-                >
+                <h2 id=":r0:" class="text-sm font-semibold text-zinc-100">
                   Workstation
                 </h2>
                 <div class="md:col-span-3">
                   <ul role="list" class="space-y-16">
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         16” MacBook Pro, M1 Max, 64GB RAM (2021)
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         I was using an Intel-based 16” MacBook Pro prior to this
                         and the difference is night and day. I&apos;ve never
                         heard the fans turn on a single time, even under the
@@ -46,31 +43,31 @@ function Uses() {
                       </p>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         13.3” MacBook Air, M1 (2021)
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         I&apos;ve never heard the fans turn on a single time,
-                        coz it doesnt have one. Even under the incredibly heavy
+                        coz it doesnt have one. even under the incredibly heavy
                         loads I put it through with our various launch
                         simulations.
                       </p>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         Apple Pro Display XDR (Standard Glass)
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         The only display on the market if you want something
                         HiDPI and bigger than 27”. When you&apos;re working at
                         planetary scale, every pixel you can get counts.
                       </p>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         IBM Model M SSK Industrial Keyboard
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         They don&apos;t make keyboards the way they used to. I
                         buy these any time I see them go up for sale and keep
                         them in storage in case I need parts or need to retire
@@ -78,20 +75,20 @@ function Uses() {
                       </p>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         Apple Magic Trackpad
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         Something about all the gestures makes me feel like a
                         wizard with special powers. I really like feeling like a
                         wizard with special powers.
                       </p>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         Herman Miller Aeron Chair
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         If I&apos;m going to slouch in the worst ergonomic
                         position imaginable all day, I might as well do it in an
                         expensive chair.
@@ -106,39 +103,36 @@ function Uses() {
               class="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40"
             >
               <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
-                <h2
-                  id=":r1:"
-                  class="text-sm font-semibold text-zinc-800 dark:text-zinc-100"
-                >
+                <h2 id=":r1:" class="text-sm font-semibold text-zinc-100">
                   Development tools
                 </h2>
                 <div class="md:col-span-3">
                   <ul role="list" class="space-y-16">
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         Sublime Text 4
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         I don&apos;t care if it&apos;s missing all of the fancy
                         IDE features everyone else relies on, Sublime Text is
                         still the best text editor ever made.
                       </p>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         iTerm2
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         I&apos;m honestly not even sure what features I get with
                         this that aren&apos;t just part of the macOS Terminal
                         but it&apos;s what I use.
                       </p>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         TablePlus
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         Great software for working with databases. Has saved me
                         from building about a thousand admin interfaces for my
                         various projects over the years.
@@ -150,22 +144,19 @@ function Uses() {
             </section>
             <section
               aria-labelledby=":r2:"
-              class="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40"
+              class="md:border-l md:border-zinc-700/40 md:pl-6"
             >
               <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
-                <h2
-                  id=":r2:"
-                  class="text-sm font-semibold text-zinc-800 dark:text-zinc-100"
-                >
+                <h2 id=":r2:" class="text-sm font-semibold text-zinc-100">
                   Design
                 </h2>
                 <div class="md:col-span-3">
                   <ul role="list" class="space-y-16">
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         Figma
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         We started using Figma as just a design tool but now
                         it&apos;s become our virtual whiteboard for the entire
                         company. Never would have expected the collaboration
@@ -178,32 +169,29 @@ function Uses() {
             </section>
             <section
               aria-labelledby=":r3:"
-              class="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40"
+              class="md:border-l md:border-zinc-700/40 md:pl-6"
             >
               <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
-                <h2
-                  id=":r3:"
-                  class="text-sm font-semibold text-zinc-800 dark:text-zinc-100"
-                >
+                <h2 id=":r3:" class="text-sm font-semibold text-zinc-100">
                   Productivity
                 </h2>
                 <div class="md:col-span-3">
                   <ul role="list" class="space-y-16">
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         Alfred
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         It&apos;s not the newest kid on the block but it&apos;s
                         still the fastest. The Sublime Text of the application
                         launcher world.
                       </p>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         Reflect
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         Using a daily notes system instead of trying to keep
                         things organized by topics has been super powerful for
                         me. And with Reflect, it&apos;s still easy for me to
@@ -212,20 +200,20 @@ function Uses() {
                       </p>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         SavvyCal
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         Great tool for scheduling meetings while protecting my
                         calendar and making sure I still have lots of time for
                         deep work during the week.
                       </p>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                      <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+                      <h3 class="text-base font-semibold tracking-tight text-zinc-100">
                         Focus
                       </h3>
-                      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                      <p class="relative z-10 mt-2 text-sm text-zinc-400">
                         Simple tool for blocking distracting websites when I
                         need to just do the work and get some momentum going.
                       </p>
