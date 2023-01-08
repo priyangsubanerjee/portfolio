773411baee6651537ed08a6ad8ee1910b1987c88
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-01-08 23:52:54 +0530

    download

--- pages/index.js
+++ pages/index.js
@@ -208,7 +208,8 @@ export default function Home() {
               </li>
               <a
                 className="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full"
-                href="/#"
+                href="https://drive.google.com/file/d/1U0eWv0CroPbGRmUrk-jdhMTQg5GtIhKV/view?usp=sharing"
+                download={true}
               >
                 Download CV
                 <svg
