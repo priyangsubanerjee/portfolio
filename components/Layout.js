0308319577cc6dd89db233e008d6ec0b86a88307
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-19 13:13:11 +0530

    pt:Layout

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ function Layout({ children }) {
     <div className="h-screen overflow-auto flex lg:px-24 bg-darkPrimary">
       <div className="h-screen w-full bg-darkSecondary border-x border-darkPrimary overflow-auto relative">
         <Navbar />
-        <div className="pt-16 lg:pt-20">{children}</div>
+        <div className="pt-10 lg:pt-20">{children}</div>
         <Footer />
       </div>
     </div>
