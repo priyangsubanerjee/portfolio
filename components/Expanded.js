5873b89b4dde9855feeb4c62625f606001a7f574
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-23 00:05:08 +0530

    react reveal removed

--- components/Expanded.js
+++ components/Expanded.js
@@ -3,8 +3,6 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState, useEffect } from "react";
 
-import { Fade } from "react-reveal";
-
 function Expanded() {
   const router = useRouter();
   const [open, setOpen] = useState(false);
@@ -34,48 +32,47 @@ function Expanded() {
         <span className="text-sm font-medium text-zinc-100">Menu</span>
         <i class="bi bi-chevron-down text-zinc-100 text-xs ml-2"></i>
       </button>
-      <Fade when={open} duration={500}>
-        {open && (
-          <div className="fixed h-full w-full z-40 inset-0 bg-black/20 backdrop-blur-sm flex p-5 flex-col">
-            <div className="h-fit w-full bg-darkPrimary border border-slate-800 rounded-3xl shrink-0">
-              <div className="flex py-4 px-6">
-                <button
-                  onClick={() => setOpen(false)}
-                  className="flex items-center justify-center h-10 w-10 bg-slate-800 rounded-full ml-auto"
-                >
-                  <i class="bi bi-x-lg text-sm text-white"></i>
-                </button>
-              </div>
-              <div className="px-6 pb-7 -mt-7">
-                <p className="text-slate-200 font-medium text-2xl">Menu</p>
-                <ul className="text-slate-400 space-y-4 mt-6">
-                  {routes.map((item, i) => {
-                    return (
-                      <li key={i}>
-                        <Link href={item.path} key={i}>
-                          <div
-                            className={`mr-5 cursor-pointer ${
-                              router.pathname == item.match
-                                ? "text-sky-300"
-                                : router.pathname.split("/")[1] ==
-                                  item.path.split("/")[1]
-                                ? "text-sky-300"
-                                : "text-slate-400"
-                            } transition-all`}
-                          >
-                            {item.title}
-                          </div>
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+
+      {open && (
+        <div className="fixed h-full w-full z-40 inset-0 bg-black/20 backdrop-blur-sm flex p-5 flex-col">
+          <div className="h-fit w-full bg-darkPrimary border border-slate-800 rounded-3xl shrink-0">
+            <div className="flex py-4 px-6">
+              <button
+                onClick={() => setOpen(false)}
+                className="flex items-center justify-center h-10 w-10 bg-slate-800 rounded-full ml-auto"
+              >
+                <i class="bi bi-x-lg text-sm text-white"></i>
+              </button>
             </div>
-            <div onClick={() => setOpen(false)} className="w-full h-full"></div>
+            <div className="px-6 pb-7 -mt-7">
+              <p className="text-slate-200 font-medium text-2xl">Menu</p>
+              <ul className="text-slate-400 space-y-4 mt-6">
+                {routes.map((item, i) => {
+                  return (
+                    <li key={i}>
+                      <Link href={item.path} key={i}>
+                        <div
+                          className={`mr-5 cursor-pointer ${
+                            router.pathname == item.match
+                              ? "text-sky-300"
+                              : router.pathname.split("/")[1] ==
+                                item.path.split("/")[1]
+                              ? "text-sky-300"
+                              : "text-slate-400"
+                          } transition-all`}
+                        >
+                          {item.title}
+                        </div>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
-        )}
-      </Fade>
+          <div onClick={() => setOpen(false)} className="w-full h-full"></div>
+        </div>
+      )}
     </div>
   );
 }
