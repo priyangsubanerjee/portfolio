536eb5c1b18d751ad9fb9544b9213b7a99015dde
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-03-11 20:09:39 +0530

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
