741407da1b38ab0dac94cec481b03646d37c41ac
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-09 21:57:45 +0530

    end

--- pages/index.js
+++ pages/index.js
@@ -41,13 +41,13 @@ export default function Home() {
           <div className="lg:w-[30%] flex flex-col items-end relative">
             <Stamp />
             <p className="text-right leading-6 lg:leading-7 mt-6 lg:mt-0 text-sm lg:text-base">
-              specialized in Web Design & Development, UX / UI, Webflow, and
+              specialized in Web Design & Development, UX / UI, Backend, and
               Front End Development.
             </p>
           </div>
         </div>
       </section>
-      <section className="min-h-[calc(100%-64px)] lg:min-h-[calc(100%-96px)] h-fit w-full bg-transparent relative flex flex-col pb-8 lg:pb-10">
+      <section className="h-auto w-full flex flex-col pb-8 lg:pb-10">
         <div className="px-6 lg:px-16 mt-24 lg:flex items-center justify-between">
           <div className="lg:w-[60%]">
             <Icon height={40} icon="icon-park-outline:quote" />
@@ -220,7 +220,26 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="h-44 w-ful"></div>
+        <div className="px-6 lg:px-16 mt-44 pb-8">
+          <h1 className="text-3xl lg:text-4xl font-poppins relative lg:leading-[1.5] leading-[1.4] max-w-2xl">
+            <div className="h-4 w-4 bg-green-500 rounded-full absolute left-0 top-0 z-10"></div>
+            <img
+              className="h-12 lg:h-14 w-12 lg:w-14 absolute top-0 left-0 rounded-full"
+              src="https://res.cloudinary.com/db9kd4qbi/image/upload/v1699544530/Personal/Screenshot_2023-11-09_at_9.11.34_PM_dfr99a.png"
+              alt=""
+            />
+            <span className="pl-14 lg:pl-16">Let&apos;s</span> talk about a
+            project, collaboration or an idea you may have
+          </h1>
+          <button className="flex group relative w-48 items-center justify-between rounded-full h-12 mt-10 text-white dark:text-black dark:bg-white bg-black">
+            <span className="text-sm md:text-base ml-5">Drop me a line</span>
+            <div className="absolute group right-[13%] hover:right-[13%] translate-x-1/2 top-1/2 -translate-y-1/2 h-3 w-3 group-hover:h-10 group-hover:w-10 text-black dark:text-white overflow-hidden bg-white dark:bg-black rounded-full ml-7 flex items-center justify-center transition-all">
+              <span className="group-hover:visible invisible">
+                <Icon icon="iconoir:arrow-tr" />
+              </span>
+            </div>
+          </button>
+        </div>
       </section>
     </>
   );
