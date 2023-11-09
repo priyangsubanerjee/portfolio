0505ee9866af2f712e3b0627e4e8500f61f0fbae
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-09 22:26:06 +0530

    sections

--- pages/index.js
+++ pages/index.js
@@ -241,6 +241,25 @@ export default function Home() {
           </button>
         </div>
       </section>
+      <footer className="mt-20 mb-10 border-t max-w-7xl mx-auto px-6 lg:px-0">
+        <div className="w-full">
+          <div className="flex items-center justify-between mt-10">
+            <span className="text-sm">
+              © 2023All Rights Reserved. Design & Coded with ❤️️
+            </span>
+            <ul className="flex items-center space-x-8 text-sm">
+              <li>LinkedIn</li>
+              <li>Github</li>
+              <li>Email</li>
+              <li>Instagram</li>
+            </ul>
+            <ul className="flex items-center space-x-8 text-sm">
+              <li>Support</li>
+              <li>Share</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
