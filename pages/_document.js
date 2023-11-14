9590583b906b33b9e28d45b2e2c78b400b9ec95a
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-14 12:41:50 +0530

    og tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
+//
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link
+          rel="shortcut icon"
+          href="https://res.cloudinary.com/db9kd4qbi/image/upload/v1699544530/Personal/Screenshot_2023-11-09_at_9.11.34_PM_dfr99a.png"
+        />
+        <link
+          rel="shortcut icon"
+          href="https://res.cloudinary.com/db9kd4qbi/image/upload/v1699544530/Personal/Screenshot_2023-11-09_at_9.11.34_PM_dfr99a.png"
+          type="image/x-icon"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="https://res.cloudinary.com/db9kd4qbi/image/upload/v1699544530/Personal/Screenshot_2023-11-09_at_9.11.34_PM_dfr99a.png"
+        />
+
         <meta
           name="description"
           content="Priyangsu Banerjee is a full stack developer and entrepreneur based in India. He is currently building VBC, where they develop technologies that empower regular people to explore tech on their own terms."
@@ -20,6 +37,32 @@ export default function Document() {
         <meta name="distribution" content="web" />
         <meta name="rating" content="general" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta
+          property="twitter:card"
+          content="https://res.cloudinary.com/db9kd4qbi/image/upload/v1699945699/Personal/Priyangsu_Banerjee_ew1cz3.png"
+        ></meta>
+        <meta
+          property="twitter:title"
+          content="Priyangsu Banerjee - Full Stack Developer"
+        ></meta>
+        <meta
+          property="twitter:description"
+          content="Priyangsu Banerjee is a full stack developer and entrepreneur based in India. He is currently building VBC, where they develop technologies that empower regular people to explore tech on their own terms."
+        ></meta>
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/db9kd4qbi/image/upload/v1699945699/Personal/Priyangsu_Banerjee_ew1cz3.png"
+        ></meta>
+        <meta
+          property="og:title"
+          content="Priyangsu Banerjee - Full Stack Developer"
+        ></meta>
+        <meta
+          property="og:description"
+          content="Priyangsu Banerjee is a full stack developer and entrepreneur based in India. He is currently building VBC, where they develop technologies that empower regular people to explore tech on their own terms."
+        />
+        <meta property="og:url" content="https://priyangsu.dev"></meta>
       </Head>
 
       <body>
