0be1e232996255c8b771fa8071bbc8092ceee0a4
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-26 17:00:31 +0530

    projects

--- static/projects.js
+++ static/projects.js
@@ -17,4 +17,29 @@ export const projects = [
     tags: ["Next JS", "Tailwind CSS", "React-Scripts"],
     year: "2022",
   },
+  {
+    title: "Kalamkari CRM",
+    description:
+      "ERP solution for the Kalamkari industry to manage their inventory, sales, and production.",
+    preview: "https://kalamkari.vercel.app",
+    github: "https://github.com/priyangsubanerjee/kalamkari",
+    tags: ["Next JS", "Tailwind CSS", "React-Scripts"],
+    year: "2022",
+  },
+  {
+    title: "DGP Care",
+    description:
+      "Developed to serve a common with a common mission to help India during the Covid crisis, brought us all together to create this platform. We try our best to amplify the need of resources while we undergo waves of Covid-19.",
+    preview: "dgpcare.vercel.app",
+    github: "https://github.com/priyangsubanerjee/dgpcare",
+    tags: ["Next JS", "Tailwind CSS", "React-Scripts"],
+    year: "2021",
+  },
+  {
+    title: "Richribbit",
+    description: "A standalone web application made for @DtoryStudios",
+    preview: "https://richribbit.vercel.app",
+    tags: ["Next JS", "Tailwind CSS", "React-Scripts"],
+    year: "2023",
+  },
 ];
