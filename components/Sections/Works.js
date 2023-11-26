983604580e654fb60af251a3d8ddd277d050f6f7
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-26 16:36:27 +0530

    projects

--- components/Sections/Works.js
+++ components/Sections/Works.js
@@ -1,4 +1,6 @@
+import { projects } from "@/static/projects";
 import { Icon } from "@iconify/react";
+import Link from "next/link";
 import React from "react";
 
 function Works() {
@@ -6,45 +8,43 @@ function Works() {
     <div id="work" className="pt-32 lg:pt-16 lg:mt-32 px-6 lg:px-16 pb-10">
       <h2 className="text-2xl lg:text-3xl font-poppins">Selected works ...</h2>
       <div className="mt-16">
-        <div className="py-10 border-b border-neutral-400 dark:border-neutral-700 flex">
-          <div className="lg:ml-20">
-            <h1 className="text-3xl lg:text-5xl">Foggies Token</h1>
-            <p className="text-sm text-neutral-600 dark:text-neutral-300 mt-5">
-              Next JS, Tailwind CSS, React-Scripts
-            </p>
+        {projects.map((project, i) => (
+          <div
+            key={i}
+            className="py-10 border-b border-neutral-400 dark:border-neutral-700 flex"
+          >
+            <div className="lg:ml-20">
+              <h1 className="text-3xl lg:text-5xl">{project.title}</h1>
+              <p className="text-sm max-w-3xl leading-7 text-neutral-600 dark:text-neutral-300 mt-5">
+                {project.description}
+              </p>
+              <div className="mt-6 flex items-center space-x-9">
+                {project.preview && (
+                  <Link href={project.preview}>
+                    <button className="flex items-center space-x-2 uppercase text-sm tracking-wider font-medium text-blue-500">
+                      <span>Preview</span>
+                      <Icon icon="ion:open-outline" />
+                    </button>
+                  </Link>
+                )}
+                {project.github && (
+                  <Link href={project.github}>
+                    <button className="flex items-center space-x-2 uppercase text-sm tracking-wider font-medium text-blue-500">
+                      <span>Github</span>
+                      <Icon icon="ion:open-outline" />
+                    </button>
+                  </Link>
+                )}
+              </div>
+            </div>
+            <div className="ml-auto whitespace-nowrap shrink-0">
+              <h1 className="text-2xl lg:text-4xl dark:text-white font-thin">
+                / {project.year}
+              </h1>
+            </div>
           </div>
-          <div className="ml-auto whitespace-nowrap shrink-0">
-            <h1 className="text-2xl lg:text-4xl dark:text-white font-thin">
-              / 2023
-            </h1>
-          </div>
-        </div>
-        <div className="py-10 border-b  border-neutral-400 dark:border-neutral-700 flex">
-          <div className="lg:ml-20">
-            <h1 className="text-3xl lg:text-5xl">Paste Jam</h1>
-            <p className="text-sm text-neutral-600 dark:text-neutral-300 dark:text-white/50 mt-5">
-              Next JS, Tailwind CSS, Firebase, Next-Auth
-            </p>
-          </div>
-          <div className="ml-auto whitespace-nowrap shrink-0">
-            <h1 className="text-2xl lg:text-4xl dark:text-white font-thin">
-              / 2022
-            </h1>
-          </div>
-        </div>
-        <div className="py-10 border-b border-neutral-400 dark:border-neutral-700 flex">
-          <div className="lg:ml-20">
-            <h1 className="text-3xl lg:text-5xl">Airshare</h1>
-            <p className="text-sm text-neutral-600 dark:text-neutral-300 mt-5 line-clamp-1">
-              Next JS, Tailwind CSS, React-Scripts, WebSockets, Cloudinary
-            </p>
-          </div>
-          <div className="ml-auto whitespace-nowrap shrink-0">
-            <h1 className="text-2xl lg:text-4xl dark:text-white font-thin">
-              / 2023
-            </h1>
-          </div>
-        </div>
+        ))}
+
         <div className="mt-16 lg:pl-20 flex items-center">
           <button className="flex group relative w-48 items-center justify-between rounded-full h-12 text-white dark:text-black dark:bg-white bg-black">
             <span className="text-sm md:text-base ml-5">See all projects</span>
---
+++ static/projects.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: "Doctor Doggy",
+    description:
+      "A standalone platform enthusiatically curated for pet parents to maintain their pet's health records, book appointments, and much more.",
+    preview: "https://doctordoggy.vet",
+    github: "",
+    tags: ["Next JS", "Tailwind CSS", "React-Scripts"],
+    year: "2023",
+  },
+  {
+    title: "Airshare",
+    description:
+      "It is a decentralized, P2P file sharing platform that allows users to share files with each other, independent of their operating systems.",
+    preview: "https://airshare.vercel.app",
+    github: "https://github.com/priyangsubanerjee/airshare",
+    tags: ["Next JS", "Tailwind CSS", "React-Scripts"],
+    year: "2022",
+  },
+];
