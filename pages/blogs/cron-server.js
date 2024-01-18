e07b42a743dcd62d07044e308909e101d4d5a995
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-01-18 23:06:58 +0530

    cron tag

--- pages/blogs/cron-server.js
+++ pages/blogs/cron-server.js
@@ -17,10 +17,7 @@ function CronServer() {
         <meta name="revisit-after" content="7 days" />
         <meta name="distribution" content="web" />
         <meta name="rating" content="general" />
-        <meta
-          property="twitter:card"
-          content="https://priyangsubanerjee.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F43efee63-de73-4b48-afd0-856f6ba29559%2Fe3e52911-1c19-44ef-b4c7-d4984e356f86%2FUntitled.png?table=block&id=675965c4-1856-4752-b9a4-fa1c612662ab&spaceId=43efee63-de73-4b48-afd0-856f6ba29559&width=1440&userId=&cache=v2"
-        ></meta>
+        <meta property="twitter:card" content="/cron-server.png"></meta>
         <meta
           property="twitter:title"
           content="Cron Server using ESP8266"
@@ -29,10 +26,7 @@ function CronServer() {
           property="twitter:description"
           content="Schedule time based triggers to run automations using ESP8266"
         ></meta>
-        <meta
-          property="og:image"
-          content="https://priyangsubanerjee.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F43efee63-de73-4b48-afd0-856f6ba29559%2Fe3e52911-1c19-44ef-b4c7-d4984e356f86%2FUntitled.png?table=block&id=675965c4-1856-4752-b9a4-fa1c612662ab&spaceId=43efee63-de73-4b48-afd0-856f6ba29559&width=1440&userId=&cache=v2"
-        ></meta>
+        <meta property="og:image" content="/cron-server.png"></meta>
         <meta property="og:title" content="Cron Server using ESP8266"></meta>
         <meta
           property="og:description"
