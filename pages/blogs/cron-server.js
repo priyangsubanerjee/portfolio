991ad8b90a9b37e08f7de42cbe42ce2da1720ef8
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-01-18 23:15:10 +0530

    redirect

--- pages/blogs/cron-server.js
+++ pages/blogs/cron-server.js
@@ -1,17 +1,11 @@
 import Head from "next/head";
-import React from "react";
-
-export function getServerSideProps() {
-  return {
-    redirect: {
-      destination:
-        "https://priyangsubanerjee.notion.site/Cron-job-using-Esp8266-0b64cbab4a7444a397fd81121a3d6550",
-      permanent: false,
-    },
-  };
-}
+import React, { useLayoutEffect } from "react";
 
 function CronServer() {
+  useLayoutEffect(() => {
+    window.location.href =
+      "https://priyangsubanerjee.notion.site/Cron-job-using-Esp8266-0b64cbab4a7444a397fd81121a3d6550";
+  }, []);
   return (
     <div>
       <Head>
