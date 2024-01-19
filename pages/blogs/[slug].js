3c365d2471542052d75931e8be095aa972ebabe6
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-01-19 14:55:05 +0530

    redirects off

---
+++ pages/blogs/[slug].js
@@ -0,0 +1,89 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import { blogs } from "@/static/blogs";
+import { Button, Spinner } from "@nextui-org/react";
+import { Icon } from "@iconify/react";
+import { useRouter } from "next/router";
+
+export function getServerSideProps({ params }) {
+  const slug = params.slug;
+  const blog = blogs.find((blog) => blog.slug === slug);
+  return {
+    props: {
+      blog: JSON.parse(JSON.stringify(blog)),
+    },
+  };
+}
+
+function Blog({ blog }) {
+  const router = useRouter();
+
+  const ProceedToBlog = () => {
+    router.push(blog.url);
+  };
+
+  const CopyUrl = () => {
+    try {
+      navigator.clipboard.writeText(blog.url);
+      document.getElementById("copytext").innerHTML = "Copied!";
+      setTimeout(() => {
+        document.getElementById("copytext").innerHTML = "Copy url";
+      }, 2000);
+    } catch (error) {
+      alert("Unsupported Browser");
+    }
+  };
+
+  return (
+    <div>
+      <img
+        src={blog.banner}
+        className="md:max-h-[200px] max-h-[100px] w-full object-cover mx-auto grayscale brightness-50"
+        alt=""
+      />
+
+      <div className="px-4">
+        <h1 className="text-2xl md:text-5xl font-bold text-center mt-10 md:mt-16">
+          {blog.title}
+        </h1>
+        <p className="text-center text-xs md:text-sm mt-2 md:mt-6 text-neutral-300 leading-6">
+          {blog.description}
+        </p>
+        <div className="flex text-sm mt-9 items-center justify-center">
+          <div className="md:flex items-center space-x-2 mr-8 hidden">
+            <span>📅</span>
+            <span>{blog.date}</span>
+          </div>
+          <div className="flex items-center space-x-2 mr-8">
+            <span>👔</span>
+            <span>@priyangsubanerjee</span>
+          </div>
+          <div className="flex items-center space-x-2">
+            <span>⏰</span>
+            <span>10 mins read</span>
+          </div>
+        </div>
+        <div className="flex items-center justify-center space-x-4 mt-10">
+          <Button
+            onClick={ProceedToBlog}
+            radius="none"
+            className="bg-slate-100 text-black rounded flex items-center"
+          >
+            <span>Read the full story</span>
+            <Icon height={16} icon="ph:arrow-right-bold" />
+          </Button>
+          <Button
+            radius="none"
+            onClick={CopyUrl}
+            className="bg-neutral-800 text-white rounded flex items-center"
+          >
+            <span id="copytext">Copy url</span>
+            <Icon height={16} icon="solar:copy-linear" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Blog;
---
+++ static/blogs.js
@@ -0,0 +1,17 @@
+export const blogs = [
+  {
+    title: "Cron Server using ESP8266",
+    description:
+      "Schedule time based triggers to run automations using ESP8266",
+    slug: "cron-server",
+    author: "@priyangsubanerjee",
+    authorProfile: "https://priyangsu.dev",
+    banner:
+      "https://t3.ftcdn.net/jpg/02/11/70/76/360_F_211707667_idWK1bCTXVQS65H2pqtoVPZnvxqhC7nk.jpg",
+    image:
+      "https://priyangsubanerjee.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F99dd0ed9-d149-499f-9c13-66dbb25eb142%2Fe3e52911-1c19-44ef-b4c7-d4984e356f86%2FUntitled.png?table=block&id=aa2cd442-8a44-4f34-b8d0-799837cff325&spaceId=99dd0ed9-d149-499f-9c13-66dbb25eb142&width=1440&userId=&cache=v2",
+    url: "https://priyangsubanerjee.notion.site/Cron-job-using-Esp8266-9b61b55ad63b46c395193df20f737547",
+    date: "18-01-2024",
+    tags: ["ESP8266", "Cron Server", "Automation", "IoT"],
+  },
+];
