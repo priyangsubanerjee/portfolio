8df0fbbb25ef992e12d907d4b6d31153c62efc6d
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-02-02 01:19:50 +0530

    Add Airshare project card to Projects page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,17 +3,32 @@ import { Icon } from "@iconify/react";
 import React from "react";
 
 export default function Projects() {
+  const projects = [
+    {
+      title: "Airshare",
+      description:
+        "A file sharing app that allows you to share files with your friends without any hassle.",
+      image:
+        "https://freebiesbug.com/wp-content/uploads/2023/09/original-phone-mockups.jpg",
+      github: "",
+      link: "",
+      cover:
+        "https://freebiesbug.com/wp-content/uploads/2023/09/original-phone-mockups.jpg",
+    },
+  ];
+
   const ProjectCard = ({ project }) => {
     return (
-      <div className="h-72 relative md:rounded-md overflow-hidden group">
-        <div className="absolute z-10 inset-0 bg-gradient-to-b from-transparent to-black flex flex-col justify-end p-5">
-          <h1 className="text-white font-semibold text-2xl font-poppins">
-            Airshare
+      <div className="h-96 relative md:rounded-xl overflow-hidden group">
+        <div className="absolute z-10 inset-0 bg-gradient-to-b from-transparent to-black flex flex-col justify-end p-10">
+          <h1 className="text-white font-light text-5xl font-poppins">
+            {project.title}
           </h1>
-          <div className="flex items-center mt-3">
-            <p className=" text-neutral-100 bg-white/10 backdrop-blur text-xs px-2 py-1 rounded">
-              January 2021
-            </p>
+          <div className="flex items-center mt-7">
+            <button className="text-black bg-white backdrop-blur text-xs px-4 py-1 rounded-full flex items-center space-x-2">
+              <span>Read case study</span>{" "}
+              <span className="text-lg">&rarr;</span>
+            </button>
             <Icon icon="mdi:github" className="text-white text-xl ml-auto" />
             <Icon
               height={24}
@@ -21,15 +36,9 @@ export default function Projects() {
               className="text-white text-xl ml-5"
             />
           </div>
-          <p className="text-xs text-neutral-200 mt-2 leading-5 group-hover:mb-0 group-hover:pt-0 -mb-[130px] pt-10 transition-all opacity-0 group-hover:opacity-100">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto
-            perspiciatis ratione ab hic odit distinctio libero dicta cumque
-            voluptatum. Tempore consequatur animi quos quia perferendis at
-            quisquam cum nemo atque.
-          </p>
         </div>
         <img
-          src="https://freemockup.net/wp-content/uploads/2019/02/001-devices-presentation-app-screens-project-showcase-graphic-psd-mockup.jpg"
+          src={project.cover}
           alt=""
           className="absolute inset-0 w-full h-full object-cover object-center transition-all group-hover:scale-105 duration-1000"
         />
@@ -43,10 +52,10 @@ export default function Projects() {
       <p className="text-center text-sm mt-3 text-neutral-600 dark:text-neutral-400">
         This is a page for my projects that keep me alive.
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-3 md:mx-16 lg:mx-24 mt-7 md:mt-12">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 md:gap-3 md:mx-16 lg:mx-24 mt-7 md:mt-12">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
       </div>
     </div>
   );
